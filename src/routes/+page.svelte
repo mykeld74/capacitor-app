bbbc749be6a2c293bf6d1f5433201273b9cf66c5
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2023-04-24 07:26:50 -0600

    Create capacitor app with geolocation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Geolocation } from "@capacitor/geolocation";
+  import { onMount } from "svelte";
+
+  interface Location {
+    coords: {
+      latitude: number | null | undefined;
+      longitude: number | null | undefined;
+      accuracy: number | null | undefined;
+      altitude: number | null | undefined;
+      altitudeAccuracy: number | null | undefined;
+      speed: number | null | undefined;
+    };
+    timestamp: number;
+  }
+  let loc: Location | null = null;
+  async function getCurrentLocation() {
+    const res = await Geolocation.getCurrentPosition();
+    loc = res;
+  }
+
+  onMount(() => {
+    getCurrentLocation();
+  });
+</script>
+
+<div>
+  <h1>Geolocation</h1>
+  <p>Your location is:</p>
+  <p>Latitude: {loc?.coords.latitude}</p>
+  <p>Longitude: {loc?.coords.longitude}</p>
+
+  <button on:click={getCurrentLocation}>Get Location</button>
+</div>
